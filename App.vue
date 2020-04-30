387a8b4c9e3b36a20b0e076675ed25b141f4fdf4
Juan Eduardo Gomez <43454459+4gmz@users.noreply.github.com>
2020-04-30 17:24:23 -0500

    configuración basica
    configuración basica

--- App.vue
+++ App.vue
@@ -1,16 +1,14 @@
 <template>
-
-<l-map style="height: 350px" :zoom="zoom" :center="center">
-<l-tile-layer :url="url"></l-tile-layer>
-<l-marker :lat-lng="markerLatLng">
-<l-popup>Hello!</l-popup>
-</l-marker>
-</l-map>
-
+  <l-map style="height: 350px" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url"></l-tile-layer>
+    <l-marker :lat-lng="markerLatLng">
+      <l-popup>Hello!</l-popup>
+    </l-marker>
+  </l-map>
 </template>
 
 <script>
-import {LMap, LTileLayer, LMarker, LPopup} from 'vue2-leaflet';
+import { LMap, LTileLayer, LMarker, LPopup } from "vue2-leaflet";
 
 export default {
   components: {
@@ -19,17 +17,16 @@ export default {
     LMarker,
     LPopup
   },
-  data () {
+  data() {
     return {
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       zoom: 8,
-      center: [47.313220, -1.319482],
-      markerLatLng: [47.313220, -1.319482]
+      center: [47.31322, -1.319482],
+      markerLatLng: [47.31322, -1.319482]
     };
   }
-}
+};
 </script>
 
 <style>
-
 </style>
